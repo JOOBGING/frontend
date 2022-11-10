acbc65c9bafeb67b8618f0859c2d5f5ed30c70e1
lizuAg <wndms058@gmail.com>
2022-11-11 02:54:45 +0900

    fontAweswome icon 추가

--- src/main.js
+++ src/main.js
@@ -2,6 +2,22 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router/index.js";
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+/* import specific icons */
+import { faChildren, faMapLocationDot } from "@fortawesome/free-solid-svg-icons";
+
+
+/* add icons to the library */
+library.add(faMapLocationDot, faChildren);
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
 Vue.config.productionTip = false;
 
 new Vue({
